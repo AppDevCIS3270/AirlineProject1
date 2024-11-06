c9446c8e11d2ac34bd6db34d8ab19251da33b4cc
Ajdin <Ajdinseho@hotmail.com>
2024-11-05 19:34:03 -0500

    Added some parameters and comments for us to use.

--- src/main/java/org/example/Customer.java
+++ src/main/java/org/example/Customer.java
@@ -1,4 +1,17 @@
 package org.example;
 
 public class Customer extends User{
+
+
+
+     /*
+    methods:
+        - search flight database for flights based off different criteria
+        like city, date and time of flight, etc. "Ask chat-gpt for more criteria"
+
+        - Be able to book a flight and add to their account
+        - be able to delete a flight from account
+        - Cant book flight more than once
+        - should warn about
+     */
 }
--- src/main/java/org/example/User.java
+++ src/main/java/org/example/User.java
@@ -1,4 +1,29 @@
 package org.example;
 
 public class User {
+    String username;
+    String password;
+    String firstName;
+    String lastName;
+    String address;
+    String zip;
+    String state;
+    String email;
+    String SSN;
+    /* need to add a field for security questions for account recovery
+     - we can probably add like 3 and store them in a two dimensional array with the question
+     - and answers
+     */
+
+
+
+
+
+
+
+
+
+
+
+
 }
